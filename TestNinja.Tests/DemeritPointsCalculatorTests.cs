e6218bf339ab56ceb3857b8e22bf92a5c54cdabc
robjsinc22 <robjsinc@gmail.com>
2018-05-24 12:53:52 +0100

    Update

---
+++ TestNinja.Tests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        private DemeritPointsCalculator _dpc = new DemeritPointsCalculator();
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)] 
+        public void CalculateDemeritPoints_SpeedIsOutOfRange_ThrowsException(int speed)
+        {
+            Assert.That(() => _dpc.CalculateDemeritPoints(speed), 
+                Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        public void CalculateDemeritPoints_SpeedIsAboveMaxSpeed_ThrowsException(int speed)
+        {
+            Assert.That(() => _dpc.CalculateDemeritPoints(301), 
+                Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        [TestCase(0,0)] //speed zero returns 0 points
+        [TestCase(64,0)] // speed within speed limit return 0 points
+        [TestCase(65,0)] // speed on speed limit return 0 points
+        [TestCase(66,0)] // 66kmp returns 0 point 
+        [TestCase(70, 1)] // 70kmh returns 1 point 
+        [TestCase(75, 2)] // 75kmh returns 2 point 
+        public void CalculateDemeritPoints_WhenCalledReturnsDemeritPoints_ReturnsZero(int speed, int result)
+        {
+            var points = _dpc.CalculateDemeritPoints(speed);
+
+            Assert.That(points, Is.EqualTo(result));
+
+        }
+
+    }
+}
---
+++ TestNinja.Tests/StackTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using TestNinja.Fundamentals;
+using NUnit.Framework;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class _stackTests
+    {
+        private Stack<string> _stack = new Stack<string>();
+
+        [Test]
+        public void Push_ArgIsNull_ThrowArguementNullException()
+        {
+            Assert.That(() => _stack.Push(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Push_ValidArg_AddArguementToList()
+        {
+            _stack.Push("abc");
+
+            Assert.That(_stack.Count, Is.EqualTo(1));
+        }
+
+        [Test] // need to check the count is zero so the above Test is valid
+        public void Count_Empty_stack_RetrunZero()
+        {
+            Assert.That(_stack.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Pop_Empty_stack_ThrowInvalidOperationException()
+        {
+            Assert.That(() => _stack.Pop(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Pop__stackWithAFewObjects_ReturnsObjectOnTop()
+        {
+            _stack.Push("a");
+            _stack.Push("b");
+            _stack.Push("c");
+
+            var result = _stack.Pop();
+
+            Assert.That(result, Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void Pop__stackWithAFewObjects_RemoveObjectOnTheTop()
+        {
+            _stack.Push("a");
+            _stack.Push("b");
+            _stack.Push("c");
+
+             _stack.Pop();
+
+            Assert.That(_stack.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Peek__EmptyStack_ThrowsInvalidOperationException()
+        {
+            Assert.That(() => _stack.Peek(), Throws.InvalidOperationException);
+        }
+
+
+        [Test]
+        public void Peek__StackWithObjects_ReturnsObjectOnTopOfStack()
+        {
+            _stack.Push("a");
+            _stack.Push("b");
+            _stack.Push("c");
+
+            var result = _stack.Peek();
+
+            Assert.That(result, Is.EqualTo("c"));
+        }
+
+
+        [Test]
+        public void Peek__StackWithObjects_DoesNotRemoveTheObjectOnTopOfStack()
+        {
+            _stack.Push("a");
+            _stack.Push("b");
+            _stack.Push("c");
+
+            _stack.Peek();
+
+            Assert.That(_stack.Count, Is.EqualTo(3));
+        }
+    }
+}
